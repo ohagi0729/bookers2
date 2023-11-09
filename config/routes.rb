07fd5a353d1180b25d54f348a7291ae9a68fbde8
EC2 Default User <ec2-user@ip-172-31-35-128.ap-northeast-1.compute.internal>
2023-11-09 05:13:34 +0000

    [update]タグ機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
+  end
   root to: "homes#top"
   get "home/about" => "homes#about", as: "about"
   resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
   resources :users, only: [:show, :edit, :update, :index]
+  get 'tagsearches/search', to: 'tagsearches#search'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
